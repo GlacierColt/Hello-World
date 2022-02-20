9395543602feb2fc52220e76c87b238590cbe85e
GlacierColt <lapel.tidbits.0c@icloud.com>
2022-02-20 12:33:20 +0200

    Hello World Update (20/02/2022 : 12:33)

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -10,12 +10,23 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            string sline;
-            Console.WriteLine("Hello World.");
-            Console.WriteLine("Please write a line to be dispalyed and saved in sline:");
-            sline = Console.ReadLine();
-            Console.WriteLine("sline: " + sline);
-            Console.ReadLine(); 
+            string s;
+            int i;
+            double d;
+            char c;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine("Enter a string");
+            s = Console.ReadLine();
+            Console.WriteLine("s: " + s);
+            Console.WriteLine();
+            Console.WriteLine("Enter a integer");
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine("i: " + i);
+            Console.ReadLine();
+
+
         }
 
     }
